16d2bd71b731cc0e1ce3de92f9b762998dcb8a8e
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2024-07-08 10:34:47 +0900

    最適化成功

--- GochiApp_4.py
+++ GochiApp_4.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from GochiSolver import GochiSolver
+
 
 st.title("ゴチバトルソルバー")
 
@@ -33,7 +35,19 @@ with tab2:
     else:
         min_weight = weight_file['price'].min()
         sum_weight = weight_file['price'].sum()
-        st.slider('スライドさせて予算を設定してください', min_value=min_weight, max_value=sum_weight)  # スライダー
+        capacity = st.slider('スライドさせて予算を設定してください', min_value=min_weight, max_value=sum_weight)  # スライダー
 
 with tab3:
-  st.markdown('### 注文の作成')
+    if weight_file is None:
+        st.markdown("- メニューデータをアップロードしてください")
+    else:
+        st.markdown('### 注文の作成')
+        solver_button = st.button("注文を作成する")
+        if solver_button:
+            gochi = GochiSolver()
+            gochi.set_data(weight_file, capacity)
+            gochi.solve()
+
+            st.markdown("### 作成結果")
+            st.markdown(f"- 注文合計額: {gochi.total}")
+            st.markdown(f"- 注文メニュー: {gochi.order_ids}")
